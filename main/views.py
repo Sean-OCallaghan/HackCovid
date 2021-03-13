9990ec4d608642e18e361b95ed99ed8a270837ba
Sean <seanoinca@gmail.com>
2021-03-13 14:13:50 -0800

    Added View

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
-def myView(request):
-    return HttpResponse('Hello, World!')
+class MyView(View):
+
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Hello, World!')
