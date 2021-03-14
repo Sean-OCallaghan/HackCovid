b21143918089a185ea48e86d516e9ec078899151
Robert <rssmith614@gmail.com>
2021-03-13 17:36:08 -0800

    read works with input from website

--- main/views.py
+++ main/views.py
@@ -1,17 +1,38 @@
+from os import read
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
 from google.cloud import storage
+import pandas as pd
+from django_project import read
+import os
 
 def mainView(request):
     if request.method =='POST':
         uploaded_file = request.FILES['document']
-        print(uploaded_file.name)
-        # client = storage.Client()
-        # bucket = client.get_bucket('waitlist_input')
-        # blob = bucket.blob(uploaded_file.name)
-        # blob.upload_from_filename(uploaded_file.name)
+        input = pd.read_csv(uploaded_file)
+        output = pd.DataFrame(input).to_csv('input.csv')
+
+        client = storage.Client()
+        bucket = client.get_bucket('waitlist_input')
+        blob = bucket.blob('input.csv')
+        blob.upload_from_filename('input.csv')
+
+        read.func()
+
+        blobs = bucket.list_blobs()
+        for blob in blobs:
+            blob.delete()
+
+        new_csv = pd.read_csv('newWaitlist.csv')
+        data_html = new_csv.to_html()
+        
+        if os.path.exists('input.csv'): os.remove('input.csv')
+        if os.path.exists('newWaitlist.csv'): os.remove('newWaitlist.csv')
+
+        return HttpResponse(data_html)
+
     return render(request,'main.html')
 
 def upload(request):
